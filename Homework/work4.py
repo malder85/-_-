e3da06f3333690f4f127f09e19557c220f2fcbb1
malder <serhiishapovalov0405@gmail.com>
2020-08-07 12:58:13 +0300

    1.commit1

--- Homework/work4.py
+++ Homework/work4.py
@@ -1,1 +1,30 @@
-a = 123
+# value = [1,2,3,4,5]
+# value = tuple(value)
+# print(type(value))
+
+
+# values = [1, 2, 3, 4, 5]
+# result = []
+# for value in values[::-1]:
+#     result.append(value)
+# print(result)
+
+# values = [1, 2, 3, 4, 5]
+# new_value = values
+# new_value.append(6)
+# print(values)
+
+# values = [0] * 6
+# values[0] = 1
+# print(values)
+
+# value = 0
+# values = [value] * 6
+# value = 1
+# print(values)
+
+my_list = [0]
+value = [0] * 3
+print(value)
+
+
