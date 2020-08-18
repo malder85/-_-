eb4548321b8bd2b7e0c96c48e247e559aabe7255
User <serhiishapovalov0405@gmail.com>
2020-08-19 00:14:44 +0300

    lesson8

---
+++ Homework/homework5.py
@@ -0,0 +1,63 @@
+# 2. Дано целое число (int). Определить сколько нулей в конце этого числа.
+
+# numb = "25102600"
+# my_list = []
+# for symbol in numb:
+#     if symbol == 0:
+#         my_list.append(symbol)
+# print(my_list)
+
+
+# не готово!!!!!!!!!
+
+
+
+######################################################
+
+# 3. Даны списки my_list_1 и my_list_2. Создать список my_result в который вначале поместить четные элементы из my_list_1
+# и потом нечетные элементы из my_list_2.
+
+# my_list_1 = ["qwerty"]
+# my_list_2 = ["asdfgh"]
+# my_result = []
+# for symbol in my_list_1:
+#     my_result.append(symbol[::2])
+# for symbol in my_list_2:
+#     my_result.append(symbol[1::2])
+# print(my_result)
+
+#####################################################
+
+# 4. Дан список my_list.Создать список new_list у которого первый элемент из my_list стоит на последнем месте.Если
+# my_list[1, 2, 3, 4], то new_list[2, 3, 4, 1]
+
+# my_list = [1, 2, 3, 4]
+# new_list = []
+# for symbol in my_list:
+#     new_list.append(symbol(0))
+# print(new_list)
+
+# не готово!!!!
+
+######################################################
+
+# 5.Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место. [1,2,3,4] -> [2,3,4,1].
+# Пересоздавать список нельзя!
+
+# my_list = [1, 2, 3, 4]
+# for symbol in my_list:
+#     my_list.insert(4, 1)
+#     my_list.remove(1)
+#     break
+# print(my_list)
+
+######################################################
+
+
+
+
+
+
+
+
+
